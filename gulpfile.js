c464f918d690f63ae6a2410e180ae3dd248fca55
Mantas Grauzinis <anonimo@inbox.lt>
2017-05-24 21:01:54 +0300

    New files added

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
 
 var SOURCEPATHS = {
-    sassSource : 'src/scss/*.scss'
+    sassSource : 'src/scss/*.scss',
+    htmlSource : 'src/*.html'
 }
 var APPPATH = {
     root : 'app/',
@@ -15,8 +17,15 @@ var APPPATH = {
 // SASS COMPILIATORIUS
 gulp.task('sass', function() {
     return gulp.src(SOURCEPATHS.sassSource)
+        .pipe(autoprefixer()) // compressed
         .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(gulp.dest(APPPATH.css))
+});
+
+// COPY HTML FILE TO APP
+gulp.task('copy', function() {
+    gulp.src(SOURCEPATHS.htmlSource) 
+        .pipe(gulp.dest(APPPATH.root))
 });
 
 // AUTOMATISKAI ATNAUJINS NARSYKLES LANGA
@@ -29,8 +38,9 @@ gulp.task('serve', ['sass'], function() {
 });
 
 // watch kad stebetu scss ir padarytu kai kazka pakeiciam
-gulp.task('watch', ['serve', 'sass'], function() {
+gulp.task('watch', ['serve', 'sass', 'copy'], function() {
     gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+    gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
 });
 
 gulp.task('default', ['watch']);
